789e19f13e057c08a1de42901046b4bd67f884c8
matthew rendely <matthew.rendely@gmail.com>
2023-04-16 10:34:53 -0700

    Implement get toys

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
+
+/* Component hierarchy sketch
+
+App (toys, handleAddToy, handleDeleteToy, handleUpdateToy [for liking])
+|── Header
+|── ToyForm (newToy, handleAddToy)
+|── ToyContainer (toys, handleDeleteToy, handleUpdateToy [for liking])
+    |–– ToyCard (toy, handleDeleteToy, handleUpdateToy [for liking])
+
+*/
+
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
 
-  function handleClick() {
+  useEffect(getToysData,[]);
+
+  function getToysData(){
+    fetch('http://localhost:3001/toys')
+      .then(r => r.json())
+      .then(toys => setToys(toys))
+  }
+   
+  function handleShowFormClick() {
     setShowForm((showForm) => !showForm);
   }
 
@@ -16,9 +36,11 @@ function App() {
       <Header />
       {showForm ? <ToyForm /> : null}
       <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
+        <button onClick={handleShowFormClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer
+        toys={toys}   
+      />
     </>
   );
 }
